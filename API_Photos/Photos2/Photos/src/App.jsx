8e176be42a620c22ded8b1213d73f326dda15055
Naiitiry <danchukroman@gmail.com>
2023-08-13 17:52:57 -0300

    colores

--- API_Photos/Photos2/Photos/src/App.jsx
+++ API_Photos/Photos2/Photos/src/App.jsx
@@ -1,10 +1,13 @@
 import { ImagenesGrid } from "./components/ImagesGrid.jsx"
+import './App.css'
 
 const App=()=>{
 
   return(
-    <div>
-      <header></header>
+    <div className="App">
+      <header>
+        <h1 className="App-h1">Galería de imágenes de JsonHolder</h1>
+      </header>
       <main>
         <ImagenesGrid/>
       </main>
