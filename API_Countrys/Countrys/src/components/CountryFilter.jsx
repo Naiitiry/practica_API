67522c48687b9c57ce1f844740feb860f5014115
Naiitiry <danchukroman@gmail.com>
2023-08-16 01:52:09 -0300

    mejoras del filtro

--- API_Countrys/Countrys/src/components/CountryFilter.jsx
+++ API_Countrys/Countrys/src/components/CountryFilter.jsx
@@ -1,21 +1,17 @@
 import './CountryFilter.css'
-
-const Filter_Country = ({name,capital,continent,lenguage,otherLenguage,population,flag,ubication}) => {
-
+const Filter_Country = ({countryData,onSelectCountry,onSelectCountry2 }) => {
+    
     return(
         <>
-            <div className="country-filter">
-                <h2 className="country-filter-name">{name}</h2>
-                <h3 className="country-filter-h3">{capital}</h3>
-                <h3 className="country-filter-h3">{continent}</h3>
-                <h3 className="country-filter-h3">{lenguage}</h3>
-                <h3 className="country-filter-h3">{otherLenguage}</h3>
-                <h3 className="country-filter-h3">{population}</h3>
-                <h3 className="country-filter-h3">{flag}</h3>
-                <span  className="country-filter-span">
-                    {ubication}
-                </span>
-            </div>
+            <li className="country-filter">
+                <h2 className="country-filter-name">{countryData.name.common}</h2>
+                <h3>Capital: {countryData.capital}</h3>
+                <h3>Region: {countryData.region}</h3>
+                <h3>Lenguaje: {countryData.languages.ara}</h3>
+                <h3></h3>
+                <button onClick={() => onSelectCountry(-1)}>Mostrar Primer País</button>
+                <button onClick={() => onSelectCountry2(1)}>Mostrar Siguiente País</button>
+            </li>
         </>
     )
 }
--- API_Countrys/Countrys/src/components/CountryGrid.jsx
+++ API_Countrys/Countrys/src/components/CountryGrid.jsx
@@ -2,25 +2,41 @@ import { useFetch } from "./useFetch.js"
 import { Filter_Country } from "./CountryFilter.jsx"
 import { useState } from "react"
 
-const CountrysWorld = (name) => {
-    const {data,loading,error} = useFetch(`https://restcountries.com/v3.1/name/${name}`)
-    const {country,setCountry} = useState('')
-    
+const CountrysWorld = () => {
+    const {data,loading,error} = useFetch(`https://restcountries.com/v3.1/all/`)
+    console.log(data);
+    //índice de paises
+    const [countryIndex,setCountryIndex] = useState(0)
+    const maxLength = data ? data.length : 0
+
+    const handleCountryPreview = () => {
+        if(countryIndex>0)
+        setCountryIndex(countryIndex-1);
+    };
+    const handleCountryNext = ()=>{
+        if(countryIndex<maxLength){
+            setCountryIndex(countryIndex+1)
+        }
+    }
     return(
         <>
             <div className="country-search-title">
                 <h1>
-                    Filtro de paises
+                    Filtro de paises: 
                 </h1>
             </div>
             <div className="country-search">
-                {loading && <h2>Cargando...</h2>}
-                {error && <h2>Error: {error.message}</h2>}
-                {data && (
-                    <Filter_Country
-                    
-                    />
-                )}
+                <ul>
+                    {loading && <h2>Cargando...</h2>}
+                    {error && <h2>Error: {error.message}</h2>}
+                    {data && (
+                        <Filter_Country
+                        countryData={data[countryIndex]}
+                        onSelectCountry={handleCountryPreview}
+                        onSelectCountry2={handleCountryNext}
+                        />
+                    )}
+                </ul>
             </div>
         </>
     )
