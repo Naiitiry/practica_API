e915de86a38479d6c695f166c3aa213d5fb4c56a
Naiitiry <you@example.com>
2023-08-18 09:01:13 -0300

    mejoras en filtrado de lenguajes

--- API_Countrys/Countrys/src/components/CountryFilter.jsx
+++ API_Countrys/Countrys/src/components/CountryFilter.jsx
@@ -1,15 +1,15 @@
 import './CountryFilter.css'
 const Filter_Country = ({countryData,onSelectCountry,onSelectCountry2, onSelectCountry3 }) => {
-    
+    const languages = Object.values(countryData.languages).slice(0,3)
     return(
         <>
             <li className="country-filter">
                 <img src={countryData.flags.png} alt="flag" className='flags'/>
                 <div className='country-filter-description'>
-                    <p>{countryData.name.common}</p>
+                    <p>País: {countryData.name.common}</p>
                     <p>Capital: {countryData.capital}</p>
                     <p>Region: {countryData.region}</p>
-                    <p>Lenguaje: {countryData.languages.ara}</p>
+                    <p>Lenguaje: {languages.join(', ')}</p>
                     <p>Población: {countryData.population}</p>
                     <p></p>
                 </div>
